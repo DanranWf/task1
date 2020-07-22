806e57265611056337fc59ccffe238ea6e7faa42
DanranWf <lianwenjing2007@126.com>
2020-07-22 16:25:41 +0800

    add pythondemo

---
+++ pythonbasic/python.py
@@ -0,0 +1,11 @@
+# demo1
+def lwj():
+    a=0
+    if a<1:
+        print(a)
+        a=a+1
+    return a>=1
+
+
+print(lwj())
+# print(lwj().a)  #========= AttributeError: 'bool' object has no attribute 'a'
